73c3ebc2ab7fe0465b8656c66b3f08d6224d7a26
alexndrperz <alan80perez@gmail.com>
2024-10-08 17:59:44 -0400

    feat:! Setup inicial de proyecto y carpetas

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace MauiApp1.ViewModels
+{
+    public partial class LoginViewModel : BaseViewModel
+    {
+        [ObservableProperty] private string _username;
+        [ObservableProperty] private string _password;
+    }
+}
